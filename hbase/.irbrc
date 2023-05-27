0c1c5fe1897b76f3f5415b085787a0bc2597d957
AnupPainuly <aanoop669@gmail.com>
2023-05-27 21:25:27 +0530

    hbase

--- hbase/.irbrc
+++ hbase/.irbrc
@@ -1,7 +1,10 @@
 require 'irb/ext/save-history'
  
-IRB.conf[:SAVE_HISTORY] = 100
+IRB.conf[:SAVE_HISTORY] = 5000
 IRB.conf[:HISTORY_FILE] = "#{ENV['HOME']}/.irb-save-history"
+def clear
+  system('clear')
+end
 
 Kernel.at_exit do
   IRB.conf[:AT_EXIT].each do |i|
--- neovim/.config/nvim/lua/darkstar/packer.lua
+++ neovim/.config/nvim/lua/darkstar/packer.lua
@@ -36,6 +36,8 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    --formatting
+    use {"jose-elias-alvarez/null-ls.nvim"}
 
     --lsp-zero
     use {
