d33d10d67d4adf80d179a68eff58993568f8a6c4
AnupPainuly <aanoop669@gmail.com>
2023-10-02 13:48:50 +0530

    feat: add custom latex snippet for hyperlink

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,7 +1,11 @@
 require("darkstar")
+
 --this is a test line
 require('dapui').setup()
 require('dap-python').setup()
+
+-- Load snippets from ~/.config/nvim/LuaSnip/
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
 
 vim.api.nvim_set_keymap("n", "<leader>dt", "DapUiToggle<CR>",{noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>db", "DapToggleBreakpoint<CR>",{noremap=true})
